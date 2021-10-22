ccc437655cc206b4fdbba8db3fafcb4ea8f99d3a
Hollywood <44089845+Vitali-Lupusor@users.noreply.github.com>
2021-10-22 02:06:57 +0100

    Added: sending API requests through proxies Added: exhaust data request, when more than 5000 entries returned

--- cmc_historical.py
+++ cmc_historical.py
@@ -2,23 +2,27 @@
 First we need to generate the date in the YYYY-MM-DD format.
 
 It should start at 2013-04-28 and run to current day.
+
+TODO:
+    1. Write a function that will ingest the retrieved data into a database
+    1. Write a function that would retrieve all data from 2013-04-28 onwards
+    1. Write a function that will only trieve the last 7 days of data
 """
 
 # Import standard modules
 import datetime as dt
 import json
 import logging
-import sys
-from os import path
-from typing import Any, Optional
+from random import randint
+from time import sleep
+from typing import Any, Dict, List, Optional
 
 # Import third-party modules
 import requests
+from bs4 import BeautifulSoup
 
 
-def get_data(
-    url: str, destination: Optional[str] = None, /, **kwargs: Any
-) -> None:
+def get_data(url: str, /, **kwargs: Any) -> List[Dict[str, Any]]:
     """
     Extract data from URL.
 
@@ -27,16 +31,13 @@ def get_data(
         url : str
             URL where to send the request to.
 
-        destination : str, None
-            Path where to write the file to.
-            If destination not provided, writes to current working directory.
-
         **kwargs : Any
             Parameters of the `request.get` method.
 
     Returns
     -------
-        NoneType
+        List[Dict[str, Any]]
+            The data from the API response.
 
     Raises
     ------
@@ -46,36 +47,83 @@ def get_data(
         FileNotFoundError
             * If bad path provided for destination.
     """
-    # Validate destination
-    basename = f"data_{dt.datetime.now():%Y%m%dT%H%M%S}.json"
-    if destination:
-        dirname = path.dirname(destination)
-        # If provided destination path is a directory, use default basename
-        if path.isdir(destination):
-            destination = path.join(destination, basename)
-        # Else, if the path is supposed to include basename, check the validity
-        # of the dirname
-        elif (not path.isdir(dirname)) and dirname:
-            logging.critical('"%s" does not exist', dirname)
-            sys.exit(1)
-    else:
-        # If no destination path provided, write to current working directory
-        # with default basename value
-        destination = basename
-
     # Send request
     response_raw = requests.get(url, **kwargs)
 
     # Validate response
-    if response_raw.status_code != 200:
+    if not response_raw.ok:
         response_raw.raise_for_status()
 
-    # Write data to disk
-    with open(destination, "w") as json_file:
-        json.dump(response_raw.json()["data"], json_file, indent=4)
+    content: list = response_raw.json()["data"]
+    start = 1
+    if kwargs.get("params"):
+        start = kwargs["params"].get("start") or start
+
+    while len(content) % 5000 == 0:
+        sleep(2)  # Wait for 2 seconds before sending the next request
+
+        # Send requests until data is exhausted
+        start += 5000
+        if kwargs["params"]:
+            kwargs["params"]["start"] = start
+        else:
+            kwargs["params"] = {"start": start}
+        response_raw = requests.get(url, **kwargs)
+
+        # Validate response
+        if not response_raw.ok:
+            response_raw.raise_for_status()
+
+        new_content = response_raw.json()["data"]
+        if new_content:
+            content += new_content
+
+    return content
 
 
-if __name__ == '__main__':
+def get_proxy() -> Optional[List[dict]]:
+    """
+    Scrape proxy server information from https://www.sslproxies.org.
+
+    Returns
+    -------
+        List[dict]
+            List of proxy servers.
+
+        NoneType
+            If failed to retrieve a response from the website.
+    """
+    # Declare variables
+    url = "https://www.sslproxies.org"
+
+    # Request proxies
+    payload = requests.get(url)
+
+    # Validate response
+    content = b""
+    if payload.ok:
+        content = payload.content
+
+    # Parse HTML
+    soup = None
+    if content:
+        soup = BeautifulSoup(content, "html.parser")
+
+    if soup:
+        header = [
+            value.text for value in soup.table.thead.tr.findAll("th")
+        ]
+        body = [
+            [tag.text for tag in value.findAll("td")]
+            for value in soup.table.tbody.findAll("tr")
+        ]
+
+        return [dict(zip(header, row)) for row in body]
+
+    return None
+
+
+if __name__ == "__main__":
     # Configure logging
     logging.basicConfig(
         format="{asctime} {levelname} {name} % {message}",
@@ -95,10 +143,6 @@ if __name__ == '__main__':
     # Start should increment 5000 each time there is more than 5k data in the
     # response
     START = 1
-    DESTINATION = (
-        f"coinmarketcap_{dt.datetime.now():%Y%m%dT%H%M%S}_"
-        f"{CONVERT.replace(',', '_')}_{QUERY_DATE}.json"
-    )
 
     parameters = {
         "convert": CONVERT,
@@ -106,21 +150,24 @@ if __name__ == '__main__':
         "limit": LIMIT,
         "start": START,
     }
+    proxies_list = get_proxy()
+    proxies = [
+        {"http": f"https://{proxy['IP Address']}:{proxy['Port']}"}
+        for proxy in proxies_list
+    ] if proxies_list else []
+    proxy = proxies[randint(0, len(proxies))] if proxies else {}
 
     message = {
         "url": URL,
-        "destination": DESTINATION,
+        "proxy": proxy,
     }
 
     try:
-        get_data(
-            URL,
-            DESTINATION,
-            params=parameters,
-        )
+        data = get_data(URL, params=parameters, proxies=proxy)
     except requests.models.HTTPError:
         message["status"] = "failure"
         logging.warning(message, exc_info=True)
     else:
         message["status"] = "success"
         logging.info("%s", json.dumps(message, indent=2))
+        print(data)  # TODO: Replace this with data the ingestion function
